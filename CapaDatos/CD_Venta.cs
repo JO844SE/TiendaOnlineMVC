04c6687f781f7af4099bbfc4a061bef3e390c4ef
José Pérez <81643322+JO844SE@users.noreply.github.com>
2024-01-24 17:10:12 -0600

    Terminado
    Se adiciono la funcionalidad de realizar pedido, es funcional

---
+++ CapaDatos/CD_Venta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace CapaDatos
+{
+    public class CD_Venta
+    {
+        public bool Registrar(Venta obj, DataTable DetalleVenta, out string Mensaje)
+        {
+            bool respuesta = false;
+            Mensaje = string.Empty;//Mensaje vacio
+
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+                {
+
+                    SqlCommand cmd = new SqlCommand("usp_RegistrarVenta", oconexion); //Ejecuta el procedimiento almacenado
+                    cmd.Parameters.AddWithValue("IdCliente", obj.IdCliente);//Paramétros de entrada
+                    cmd.Parameters.AddWithValue("TotalProducto", obj.TotalProducto);
+                    cmd.Parameters.AddWithValue("MontoTotal", obj.MontoTotal);
+                    cmd.Parameters.AddWithValue("Contacto", obj.Contacto);
+                    cmd.Parameters.AddWithValue("IdDistrito", obj.IdDistrito);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Direccion", obj.Direccion);
+                    cmd.Parameters.AddWithValue("IdTransaccion", obj.IdTransaccion);
+                    cmd.Parameters.AddWithValue("DetalleVenta", DetalleVenta);
+
+                    //Paramétros de salida 
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;//tipo de comando, procedimiento almacenado
+
+                    oconexion.Open();//Abrimos la conexion
+                    cmd.ExecuteNonQuery();
+
+                    respuesta = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                respuesta = false;
+                Mensaje = ex.Message;
+            }
+            return respuesta;
+        }
+    }
+}
